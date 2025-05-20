af8c458c612b4feacf5c0aa06a70a33625455cfb
Harsh <harshpandey2060@gmail.com>
2025-05-20 11:20:30 +0530

    admin panal work fixed

--- AdminPanel/src/App.js
+++ AdminPanel/src/App.js
@@ -53,7 +53,6 @@ function App() {
               flexGrow: 1,
               p: 3,
               width: { sm: `calc(100% - ${drawerWidth}px)` },
-              ml: { sm: `${drawerWidth}px` },
               mt: 8
             }}
           >
